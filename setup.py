3780626dd4a4c046131a49a7dc30ea66b0d659a1
kristjan <vilgo44@hotmail.com>
2019-07-18 16:08:22 +0300

    EDX client moved to separate package and published for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EDX',
+    version='0.0.2',
+    packages=['EDX'],
+    url='https://github.com/Haigutus/EDX',
+    license='GPL2',
+    author='kristjan',
+    author_email='kristjan.vilgo@gmail.com',
+    description='EDX SOAP API implementation in python'
+)
