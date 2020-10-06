7d5ceb87a2d38e4678815f1cf136f7edc54343cd
kristjan <vilgo44@hotmail.com>
2020-10-07 00:06:00 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='EDX',
-    version='0.0.6',
+    version='0.0.7',
     packages=['EDX'],
     long_description=long_description,
     long_description_content_type="text/markdown",
