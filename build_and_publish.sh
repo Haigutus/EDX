cbb6555665872b4f69f77fa9c3ab342d14194bdd
kristjan <vilgo44@hotmail.com>
2020-10-06 23:54:36 +0300

    Minor corrections

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,3 +1,7 @@
-rm dist
+rm -r ./dist
+rm -r ./build
+rm -r ./EDX.egg-info
 git push
+python3 setup.py sdist bdist_wheel
+#keyring set https://upload.pypi.org/legacy/ your-username
 twine upload dist/*
