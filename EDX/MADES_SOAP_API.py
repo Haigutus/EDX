7a31734caf08879c4fb0cd00ec85c780f7caa655
kristjan.vilgo <kristjan.vilgo@gmail.com>
2024-01-03 08:56:36 +0200

    1. Extended with wsse auth support, you can now pass in any zeep.wsse method to wsse. 2. Fixed bug of class name conflict 3. Refactored some code

--- EDX/MADES_SOAP_API.py
+++ EDX/MADES_SOAP_API.py
@@ -10,7 +10,7 @@
 #-------------------------------------------------------------------------------
 from requests import Session
 from requests.auth import HTTPBasicAuth
-from zeep import Client
+from zeep import Client as SOAPClient
 from zeep.transports import Transport
 from zeep.plugins import HistoryPlugin
 from lxml import etree
@@ -25,54 +25,43 @@ class Client:
     This class is designed to create a client for interacting with an EDX MADES SOAP web service.
 
     The client is initialized with a server address and optional parameters for authentication,
-    debugging, and SSL verification. It sets up a session with the server and configures
-    the client for the specified web service using WSDL (Web Services Description Language).
+    debugging, SSL verification, and WS-Security. It sets up a session with the server and
+    configures the client for the specified web service using WSDL (Web Services Description Language).
 
     Attributes:
-        service: The raw SOAP service object which can be used to make requests to the web service.
+        service: The raw SOAP service object which can be used to make requests to the web service, should not be used as methods in this class already expose all webservice functions.
         history: If debugging is enabled, this attribute stores the history of transactions.
         debug: Boolean flag to enable or disable debugging.
 
     Args:
-        server (str): The server address or IP where the web service is hosted.
-                      This parameter is mandatory.
-        username (str, optional): The username for HTTP basic authentication.
-                                  Defaults to None.
-        password (str, optional): The password for HTTP basic authentication.
-                                  Defaults to None.
-        debug (bool, optional): Flag to enable or disable debugging features.
-                                Defaults to False.
-        verify (bool, optional): Flag to enable or disable SSL verification.
-                                 Defaults to False.
-        auth (requests.auth.AuthBase, optional): Custom authentication mechanism. Any auth supported by "requests.auth" can be used.
-                                                 Defaults to None.
+        server (str): The server address or IP where the web service is hosted. This parameter is mandatory.
+        username (str, optional): The username for HTTP basic authentication. Defaults to None.
+        password (str, optional): The password for HTTP basic authentication. Defaults to None.
+        debug (bool, optional): Flag to enable or disable debugging features. Defaults to False.
+        verify (bool/str, optional): Flag to enable SSL verification or a path to a CA_BUNDLE file or directory with certificates. Defaults to False.
+        auth (requests.auth.AuthBase, optional): Custom HTTP authentication mechanism. Any auth supported by "requests.auth" can be used. Defaults to None.
+        wsse (zeep.wsse.WSSE, optional): Web Service Security object to add security tokens to SOAP messages. Defaults to None.
 
     Methods:
-        _print_last_message_exchange: Prints the last sent and received SOAP messages.
-                                      Works only if debug mode is enabled.
-        connectivity_test: Performs a connectivity test with the given receiver EIC and business type.
-                           Returns a message ID.
-        send_message: Sends a message to the specified receiver with given parameters.
-                      Returns a message ID.
-        check_message_status: Checks the status of a message using its message ID.
-                              Returns the status of the message.
-        receive_message: Receives a message of a specified business type.
-                         Returns the received message and the remaining message count.
-        confirm_received_message: Confirms the receipt of a message using its message ID.
-                                  Returns the same message ID as confirmation.
+        _print_last_message_exchange: Prints the last sent and received SOAP messages. Works only if debug mode is enabled.
+        connectivity_test: Performs a connectivity test with the given receiver EIC and business type. Returns a message ID.
+        send_message: Sends a message to the specified receiver with given parameters. Returns a message ID.
+        check_message_status: Checks the status of a message using its message ID. Returns the status of the message.
+        receive_message: Receives a message of a specified business type. Returns the received message and the remaining message count.
+        confirm_received_message: Confirms the receipt of a message using its message ID. Returns the same message ID as confirmation.
 
     Notes:
         - If 'username' is provided, HTTP basic authentication is set up with 'username' and 'password' and will perform preemptive auth.
         - If 'auth' is provided, it is used as the authentication mechanism.
+        - If 'wsse' is provided, it will be used to add security tokens to the SOAP messages, enabling WS-Security.
         - Enabling 'debug' logs detailed information about the raw SOAP requests and responses.
     """
 
-    def __init__(self, server, username=None, password=None, debug=False, verify=False, auth=None):
+    def __init__(self, server, username=None, password=None, debug=False, verify=False, auth=None, wsse=None):
 
         """At minimum server address or IP must be provided"""
 
-        wsdl = '{}/ws/madesInWSInterface.wsdl'.format(server)
-
+        # Authenticate HTTP session
         session = Session()
         session.verify = verify
 
@@ -84,19 +73,23 @@ class Client:
             session.auth = auth
 
         transport = Transport(session=session)
+
+        # Add history plugin for debug
         self.history = HistoryPlugin()
         self.debug = debug
 
-        if debug:
-            client = Client(wsdl, transport=transport, plugins=[self.history])
-        else:
-            client = Client(wsdl, transport=transport)
+        plugins = []
 
-        client.debug = debug
+        if debug:
+            plugins.append(self.history)
+
+        # Create SOAP client
+        wsdl = '{}/ws/madesInWSInterface.wsdl'.format(server)
+        client = SOAPClient(wsdl, transport=transport, plugins=plugins, wsse=wsse)
 
         self.service = client.create_service(
-            '{http://mades.entsoe.eu/}MadesEndpointSOAP12',
-            '{}/ws/madesInWSInterface'.format(server))
+            binding_name='{http://mades.entsoe.eu/}MadesEndpointSOAP12',
+            address='{server}/ws/madesInWSInterface')
 
     def _print_last_message_exchange(self):
         """Prints out last sent and received SOAP messages"""
